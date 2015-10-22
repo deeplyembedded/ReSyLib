17fc1d83cb4c977ab62dfb8cac30c6bb601d3015
Viktor <djvw1983@googlemail.com>
2015-10-22 12:23:36 +0200

    First declarations of gpio header files, not tested/compiled yet

---
+++ src/RSL/GPIO_Digital.h
@@ -0,0 +1,22 @@
+namespace RLS {
+
+class GPIO_Digital {
+public:
+	enum State : signed int {
+				LOW = 0,
+				HIGH = 1,
+				FLOATING = -1
+			};
+
+	GPIO_Digital(GPIOPin pin);
+	
+	void setDirection(Direction direction);
+	Direction getDirection();
+	
+	void setValue(State value);
+	State getValue();
+	
+	~GPIO_Digital();
+};
+
+			
