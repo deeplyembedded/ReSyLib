2b60f06308c447e7781731e40cffe4d21150312e
Viktor <djvw1983@googlemail.com>
2015-11-04 11:30:38 +0100

    Added small test application for GPIO_Digital class, yet to be tested on a beaglebone

---
+++ GPIO_test/blinking_led.cpp
@@ -0,0 +1,12 @@
+#include <GPIO_Digital.h>
+
+int main(int argc, char **argv) {
+	using namespace RSL;
+
+	GPIO_Digital ledPin(GPIOPin::P9_12);
+	ledPin.setDirection(Direction::OUTPUT);
+
+	while (true) {
+		ledPin.setValue((GPIO_Digital::State)!ledPin.getValue());
+	}
+}
