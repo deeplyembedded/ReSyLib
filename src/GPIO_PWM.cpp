7ed9a8dda6486550ad0190de8572105df6915e06
Yannic <yannic.wilkening@haw-hamburg.de>
2015-11-10 11:26:15 +0100

    - implementation of GPIO_PWM

---
+++ src/GPIO_PWM.cpp
@@ -0,0 +1,108 @@
+/*
+ * GPIO_PWM.cpp
+ *
+ *  Created on: 07.11.2015
+ *      Author: Yannic
+ */
+
+#include "GPIO_PWM.h"
+#include <string>
+
+namespace RSL {
+
+using namespace std;
+using namespace RSL;
+
+// todo check pin form usability
+GPIO_PWM::GPIO_PWM(GPIOPin pin) {
+	std::fstream fileStream;
+
+	this->pin = pin;
+
+	fileStream.open((SLOT_FILE_PATH).c_str(), fstream::out | fstream::in);
+	fileStream << "am33xx_pwm";
+	fileStream << "bone_pwm_" + getPinNameFromEnum(pin);
+	fileStream.flush();
+	fileStream.close();
+
+	dutyFileStream.open((PWM_PATH + getPinNameFromEnum(pin) + "/duty").c_str(), fstream::out | fstream::in);
+	periodFileStream.open((PWM_PATH + getPinNameFromEnum(pin) + "/period").c_str(), fstream::out | fstream::in);
+	polarityFileStream.open((PWM_PATH + getPinNameFromEnum(pin) + "/polarity").c_str(), fstream::out | fstream::in);
+	activeFileStream.open((PWM_PATH + getPinNameFromEnum(pin) + "/run").c_str(), fstream::out | fstream::in);
+
+	disablePWM();
+	setPeriod(20000000);
+	setDuty(1000000 + (1000000 * 0.5));
+	setPolarity(GPIO_PWM::Polarity::PHASED);
+
+}
+
+GPIO_PWM::~GPIO_PWM() {
+	dutyFileStream.close();
+	periodFileStream.close();
+	polarityFileStream.close();
+	activeFileStream.close();
+}
+
+void GPIO_PWM::setPeriod(unsigned int period) {
+	this->period = period;
+	periodFileStream << period;
+	periodFileStream.flush();
+}
+
+unsigned int GPIO_PWM::getPeriod() {
+	return period;
+}
+
+void GPIO_PWM::setDuty(unsigned int duty) {
+	this->duty = duty;
+	dutyFileStream << duty;
+	dutyFileStream.flush();
+}
+
+unsigned int GPIO_PWM::getDuty() {
+	return this->duty;
+}
+
+void GPIO_PWM::setPolarity(Polarity polarity) {
+	this->polarity = polarity;
+	polarityFileStream << polarity;
+	polarityFileStream.flush();
+}
+
+GPIO_PWM::Polarity GPIO_PWM::getPolarity() {
+	return polarity;
+}
+
+void GPIO_PWM::enablePWM() {
+	this->pwmState = GPIO_PWM::PWMState::ON;
+	activeFileStream << GPIO_PWM::PWMState::ON;
+	activeFileStream.flush();
+}
+
+void GPIO_PWM::disablePWM() {
+	this->pwmState = GPIO_PWM::PWMState::OFF;
+	activeFileStream << GPIO_PWM::PWMState::OFF;
+	activeFileStream.flush();
+}
+
+GPIO_PWM::PWMState GPIO_PWM::getPWMState() {
+	return this->pwmState;
+}
+
+string getPinNameFromEnum(GPIOPin pin) {
+	switch (pin) {
+		case P8_13: return "P8_13";
+		case P8_19: return "P8_19";
+		case P9_14: return "P9_14";
+		case P9_16: return "P9_16";
+		case P9_21: return "P9_21";
+		case P9_22: return "P9_22";
+		case P9_42: return "P9_42";
+	};
+
+	return "";
+}
+
+}
+
--- src/GPIO_PWM.h
+++ src/GPIO_PWM.h
@@ -1,25 +1,44 @@
+#include <fstream>
+#include <string>
+
 namespace RSL {
-using namespace RSL;
-class GPIO_PWM {
-public:
-	enum Polarity
-		: unsigned int {
-			PHASED = 1, DEPHASED = 0
+
+	using namespace RSL;
+	using namespace std;
+
+	class GPIO_PWM: public RSL_core::GPIO {
+		public:
+			enum Polarity: unsigned int {PHASED = 1, DEPHASED = 0};
+			enum PWMState: unsigned int {ON=1, OFF=0};
+
+			GPIO_PWM(GPIOPin pin);
+			~GPIO_PWM();
+
+			void setPeriod(unsigned int period);
+			unsigned int getPeriod();
+
+			void setDuty(unsigned int duty);
+			unsigned int getDuty();
+
+			void setPolarity(Polarity polarity);
+			Polarity getPolarity();
+
+			void enablePWM();
+			void disablePWM();
+
+			PWMState getPWMState();
+		private:
+			const string SLOT_FILE_PATH = "/sys/devices/bone_capemgr.9/slots";
+			const string PWM_PATH = "/sys/devices/ocp.3/";
+
+			GPIOPin pin;
+			Polarity polarity;
+			unsigned int period;
+			unsigned int duty;
+			PWMState pwmState;
+			std::fstream dutyFileStream, periodFileStream, polarityFileStream, activeFileStream;
+
+			string getPinNameFromEnum(GPIOPin pin);
 	};
 
-	virtual GPIO_PWM(GPIOPin pin);
-
-	virtual void setPeriod(unsigned int period);
-	virtual unsigned int getPeriod();
-
-	virtual void setDuty(unsigned int period);
-	virtual unsigned int getDuty();
-
-	virtual void setPolarity(Polarity polarity);
-	virtual Polarity getPolarity();
-
-	virtual ~GPIO_PWM();
 };
-
-}
-;
