e3ed7bfbc8ad9aead33a04660040eaefe302a306
Marc <marc.kirchner@haw-hamburg.de>
2015-11-17 16:07:53 +0100

    LED aktualisiert

---
+++ src/periphery/LED/LED.h
@@ -0,0 +1,59 @@
+//
+// Created by Marc on 09.11.2015.
+//
+
+    #ifndef RESYLIB_LED_H
+    #define RESYLIB_LED_H
+
+    //#include "../actor.h"
+	#include "../../GPIO.h"
+	#include "../../GPIO_Digital.h"
+	#include "../../HWManager.h"
+
+    namespace RSL {
+    using namespace RSL_core;
+        class LED {
+
+        private:
+
+            /**
+             * remembers whether the LED is on (true = on)
+             */
+            bool _isOn;
+
+            /**
+             * The GPIO pin used by the LD.
+             */
+            GPIO_Digital* _gpio_d;
+
+        public:
+
+            /**
+             * Constructor for the LED, the given pin will be registered.
+             */
+            LED(GPIOPin pin);
+
+            /**
+             * Switches the LED on.
+             */
+            void on();
+
+            /**
+             * Switches the LED off.
+             */
+            void off();
+
+            /**
+             * If on switches off, if off switches on.
+             */
+            void toggle();
+
+            /**
+             * Destructor that unregisters the GPIO-pin
+             */
+            ~LED();
+        };
+
+    }
+
+    #endif //RESYLIB_LED_H
